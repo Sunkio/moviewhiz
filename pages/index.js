c47f916c5616621b9cc5188c1ad9d2046e2edb19
Tanja Schmidt <taschmi@gmx.net>
2023-02-26 13:25:00 +0100

    add sub-container to index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Footer from '../components/Footer';
+import Contact from './contact';
 import { useState } from 'react';
 
 const Home = () => {
@@ -38,44 +39,46 @@ const Home = () => {
           <div className="header-title">
             <h1 className="neon-text">MovieWhiz</h1>
           </div>
-          <div className="header-subtitle">
-            <h2>Let AI Pick the Perfect Movie - Simply Input Your Prompt & Press "Generate"!</h2>
-            <p>
-                Simply enter your chosen theme, genre, actors and/or who you're gonna watch it with into the prompt
-                 box and let MovieWhiz pick the perfect movie for you.
-            </p>
-            <p>
-              If you don't have any specific preferences, don't worry - simply leave the prompt empty and MovieWhiz will
-              select a movie that you're sure to love!
-            </p>
-          </div>
-            <div className="prompt-container">
-                <textarea
-                    className="prompt-box"
-                    placeholder="a rom-com with a deeper meaning that impresses my mother-in-law"
-                    value={userInput}
-                    onChange={(e) => {setUserInput(e.target.value)}}
-                />
-            </div>
-            <div className="prompt-buttons ">
-                <a className={isGenerating ? 'generate-button loading neon-box' : 'generate-button neon-box'} onClick={callGenerateEndpoint}>
-                    <div className="generate">
-                        {isGenerating ? <span className="loader"></span> : <p className="grow">Generate</p>}
-                    </div>
-                </a>
-            </div>
-            {apiOutput && (
-                <div className="output">
-                    <div className="output-header-container">
-                        <div className="output-header">
-                            <h2>Your Recommendation</h2>
+            <div className="sub-container">
+              <div className="header-subtitle">
+                <h2>Let AI Pick the Perfect Movie - Simply Input Your Prompt & Press "Generate"!</h2>
+                <p>
+                    Simply enter your chosen theme, genre, actors and/or who you're gonna watch it with into the prompt
+                     box and let MovieWhiz pick the perfect movie for you.
+                </p>
+                <p>
+                  If you don't have any specific preferences, don't worry - simply leave the prompt empty and MovieWhiz will
+                  select a movie that you're sure to love!
+                </p>
+              </div>
+                <div className="prompt-container">
+                    <textarea
+                        className="prompt-box"
+                        placeholder="a rom-com with a deeper meaning that impresses my mother-in-law"
+                        value={userInput}
+                        onChange={(e) => {setUserInput(e.target.value)}}
+                    />
+                </div>
+                <div className="prompt-buttons ">
+                    <a className={isGenerating ? 'generate-button loading neon-box' : 'generate-button neon-box'} onClick={callGenerateEndpoint}>
+                        <div className="generate">
+                            {isGenerating ? <span className="loader"></span> : <p className="grow">Generate</p>}
+                        </div>
+                    </a>
+                </div>
+                {apiOutput && (
+                    <div className="output">
+                        <div className="output-header-container">
+                            <div className="output-header">
+                                <h2>Your Recommendation</h2>
+                            </div>
+                        </div>
+                        <div className="output-content">
+                            <p>{apiOutput}</p>
                         </div>
                     </div>
-                    <div className="output-content">
-                        <p>{apiOutput}</p>
-                    </div>
-                </div>
-            )}
+                )}
+            </div>
         </div>
         <Footer />
       </div>
