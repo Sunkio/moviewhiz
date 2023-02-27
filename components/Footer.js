222189c882cc8b6d227205e3a5acb4ef3728d829
Tanja Schmidt <taschmi@gmx.net>
2023-02-27 19:52:19 +0100

    add Home link, improve link styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,15 +3,17 @@ import Link from "next/link";
 function Footer() {
   return (
     <footer className="footer">
+
+      <p><Link className="footer-link " href="/">Home</Link></p>
+      <p><Link className="footer-link" href="/contact">Contact</Link></p>
+      <p><Link className="footer-link" href="/legal">Legal Disclaimer</Link></p>
       <p className="footer-text">
         Made with ❤️ by{' '}
         <a
           className="footer-link"
           href="https://find.tanjaschmidt.com" target="_blank">
           Tanja</a>
-        </p>
-      <p><Link className="footer-link" href="/contact">Contact</Link></p>
-      <p><a className="footer-link" href="">Legal Disclaimer</a></p>
+      </p>
       <p className="" id="last">Buy me a <a className="footer-link" href="https://www.buymeacoffee.com/tanjaS" target="_blank">Coffee</a></p>
     </footer>
     );
