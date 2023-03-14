9e850fe925f994c861848fecfcee460747d1f0cd
Tanja Schmidt <taschmi@gmx.net>
2023-03-14 10:54:10 +0100

    update og pic - close up content

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,13 @@ export default function Document() {
     <Html>
       <Head>
         <meta property="og:title" content="MovieWhiz" key="title"/>
-        <meta property="og:description" content="Let AI pick the perfect movie" key="description"/>
+        <meta property="og:description" content="Let AI pick the perfect movie for you - simply enter your prompt and press generate" key="description"/>
         <meta
           property="og:image"
-          content="https://moviewhiz.xyz/moviewhiz-og.png"
+          content="https://moviewhiz.xyz/moviewhiz-og.jpg"
         />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://moviewhiz.xyz" />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
       <body>
